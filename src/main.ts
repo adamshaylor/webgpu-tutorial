a8df04d3c29f59c87df06fdb0310ed9616fe5072
Adam Shaylor <business@adamshaylor.com>
2024-01-17 16:14:29 +0100

    Pull draw code into a function
    I might regret this. I was starting to go down the path of trying to
    redraw when the aspect ratio changed with some kind of uniform. I’ve
    given up on that and am going to move onto the next step in the
    tutorial, but I’m keeping the `draw()` function as-is under the
    assumption that eventually I’ll need a render loop of some kind anyway.

--- src/main.ts
+++ src/main.ts
@@ -10,87 +10,88 @@ const resizeCanvas = (): void => {
   canvas.height = Math.floor(canvas.clientHeight * scale);
 };
 
-resizeCanvas();
-window.addEventListener('resize', () => {
-  resizeCanvas();
-});
-
 const initResult = await initWebGpu(canvas);
-
 if (initResult.ok === false) {
   alert(initResult.error.message);
   throw initResult.error;
 }
-
 const gpu = initResult.value;
 
-const pass = gpu.encoder.beginRenderPass({
-  colorAttachments: [
-    {
-      view: gpu.context.getCurrentTexture().createView(),
-      loadOp: 'clear',
-      storeOp: 'store',
-      clearValue: { r: 0, g: 0, b: 0.4, a: 1 },
+const draw = () => {
+  const pass = gpu.encoder.beginRenderPass({
+    colorAttachments: [
+      {
+        view: gpu.context.getCurrentTexture().createView(),
+        loadOp: 'clear',
+        storeOp: 'store',
+        clearValue: { r: 0, g: 0, b: 0.4, a: 1 },
+      },
+    ],
+  });
+
+  // prettier-ignore
+  const vertices = new Float32Array([
+  //   X,    Y,
+    -0.8, -0.8, // Triangle 1 (Blue)
+     0.8, -0.8,
+     0.8,  0.8,
+  
+    -0.8, -0.8, // Triangle 2 (Red)
+     0.8,  0.8,
+    -0.8,  0.8,
+  ])
+
+  const vertexBuffer = gpu.device.createBuffer({
+    label: 'Cell vertices',
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+
+  gpu.device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+  const numDimensions = 2;
+  const arrayStride = numDimensions * vertices.BYTES_PER_ELEMENT;
+
+  const vertexBufferLayout: GPUVertexBufferLayout = {
+    arrayStride,
+    attributes: [
+      {
+        format: `float32x${numDimensions}`,
+        offset: 0,
+        shaderLocation: 0,
+      },
+    ],
+  };
+
+  const cellShaderModule = gpu.device.createShaderModule({
+    label: 'Cell shader',
+    code: cellShader,
+  });
+
+  const cellPipeline = gpu.device.createRenderPipeline({
+    label: 'Cell pipeline',
+    layout: 'auto',
+    vertex: {
+      module: cellShaderModule,
+      entryPoint: 'vertexMain',
+      buffers: [vertexBufferLayout],
     },
-  ],
-});
-
-// prettier-ignore
-const vertices = new Float32Array([
-//   X,    Y,
-  -0.8, -0.8, // Triangle 1 (Blue)
-   0.8, -0.8,
-   0.8,  0.8,
-
-  -0.8, -0.8, // Triangle 2 (Red)
-   0.8,  0.8,
-  -0.8,  0.8,
-])
-
-const vertexBuffer = gpu.device.createBuffer({
-  label: 'Cell vertices',
-  size: vertices.byteLength,
-  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-});
-
-gpu.device.queue.writeBuffer(vertexBuffer, 0, vertices);
-
-const numDimensions = 2;
-const arrayStride = numDimensions * vertices.BYTES_PER_ELEMENT;
-
-const vertexBufferLayout: GPUVertexBufferLayout = {
-  arrayStride,
-  attributes: [
-    {
-      format: `float32x${numDimensions}`,
-      offset: 0,
-      shaderLocation: 0,
+    fragment: {
+      module: cellShaderModule,
+      entryPoint: 'fragmentMain',
+      targets: [{ format: gpu.format }],
     },
-  ],
+  });
+
+  pass.setPipeline(cellPipeline);
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.draw(vertices.length / numDimensions);
+  pass.end();
+  gpu.device.queue.submit([gpu.encoder.finish()]);
 };
 
-const cellShaderModule = gpu.device.createShaderModule({
-  label: 'Cell shader',
-  code: cellShader,
+resizeCanvas();
+draw();
+window.addEventListener('resize', () => {
+  resizeCanvas();
 });
-
-const cellPipeline = gpu.device.createRenderPipeline({
-  label: 'Cell pipeline',
-  layout: 'auto',
-  vertex: {
-    module: cellShaderModule,
-    entryPoint: 'vertexMain',
-    buffers: [vertexBufferLayout],
-  },
-  fragment: {
-    module: cellShaderModule,
-    entryPoint: 'fragmentMain',
-    targets: [{ format: gpu.format }],
-  },
-});
-
-pass.setPipeline(cellPipeline);
-pass.setVertexBuffer(0, vertexBuffer);
-pass.draw(vertices.length / numDimensions);
-pass.end();
-gpu.device.queue.submit([gpu.encoder.finish()]);
